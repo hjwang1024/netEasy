194fa81219b2f1546d8d83ed44924d84d1480e1d
hongjie.wang <hjwang1024@163.com>
2022-07-14 11:15:17 +0800

    build: init config

---
+++ .stylelintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties', 'stylelint-scss'],
+  rules: {
+    'plugin/declaration-block-no-ignored-properties': true,
+    'comment-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'function-name-case': 'lower',
+    'no-descending-specificity': null,
+    'no-invalid-double-slash-comments': null,
+    'block-no-empty': null,
+    'value-keyword-case': null,
+    'rule-empty-line-before': ['always', { except: ['after-single-line-comment', 'first-nested'] }],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'order/properties-order': null,
+    'selector-pseudo-element-colon-notation': null,
+  },
+  ignoreFiles: ['node_modules/**/*', 'build/**/*', 'dist/**/*'],
+};
